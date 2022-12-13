0a8e21b458fcde02ecef62314f19cf94f01cc30e
sailorey <reyyvit@gmail.com>
2022-12-13 15:39:44 -0600

    completed

--- capstone-3/src/components/Form.js
+++ capstone-3/src/components/Form.js
@@ -16,6 +16,7 @@ export default function Form() {
             .catch(err => console.error(err))
     }, [meme])
     
+    
     const [allMemeImages, setAllMemeImages] = useState()
 
     useEffect(() => {
